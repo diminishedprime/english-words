73ffcb3b6a5fb12dc1a8383396cda1d7840c0fba
Matt <matthewjhamrick@gmail.com>
2016-09-17 17:25:03 -0500

    Here's a try?

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject english-words "1"
+  :description "english words is a hacky way to get a big ole word list to clojure programs that need one, without using slurp."
+  :url "https://github.com/diminishedprime/anagrams"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
